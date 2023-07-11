7dfc0a63dcf54c8d9786b92e3fcb0ef3702e0475
GIA KHANH <giakhanh27403@gmail.com>
2023-07-11 22:41:20 +0700

    check

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000+
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
 
 app.get('/tin-tuc', (req, res) => {
-var a = 1
-var b = 2
-
-var c = a + b
   res.send('Hello World!')
 })
 
